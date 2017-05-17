2e3febb46356d029bbd56395f3ebeee875c350d7
dung07t2 <dung07t2@gmail.com>
2017-05-18 04:13:27 +0800

    fixed staticfiles_dirs

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -185,17 +185,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = [
+    "/static",
+]
 #print(STATICFILES_DIRS)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(PROJECT_ROOT, 'www')
+STATIC_ROOT =  BASE_DIR + '/static'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
