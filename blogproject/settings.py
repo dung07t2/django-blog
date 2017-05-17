8fa4301a695a5dc394d0517f13b7a14b0c0a3d27
dung07t2 <dung07t2@gmail.com>
2017-05-18 03:17:45 +0800

    updated staticfiles dirs, not user static_root

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "www"),
+    os.path.join(BASE_DIR, "static"),
 )
 #print(STATICFILES_DIRS)
 STATICFILES_FINDERS = (
@@ -195,7 +195,7 @@ STATICFILES_FINDERS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  BASE_DIR + '/static'
+#STATIC_ROOT =  BASE_DIR + '/static'
 
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = '/home/rayleigh/python/workspace/media'
