041c7a9d3afb40ed4935b71a0e41e481f7715164
dung07t2 <dung07t2@gmail.com>
2017-05-18 00:07:44 +0800

    add STATICFILES_FINDERS

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -176,11 +176,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, "static"),
-    #'/var/www/static/',
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
 #print(STATICFILES_DIRS)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 STATIC_URL = '/static/'
 STATIC_ROOT =  BASE_DIR + '/static'
