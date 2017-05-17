a7b19099d157a7d7930747a37e2f3885ad24a58b
dung07t2 <dung07t2@gmail.com>
2017-05-18 04:18:01 +0800

    removed whitenose

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
 
 
