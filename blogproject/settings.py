ea8e904b02d9765020d62d35851babb17c61f378
dunglh <dung07t2@gmail.com>
2017-05-26 00:17:41 +0800

    change static path

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "blogproject/static"),
 ]
 # #print(STATICFILES_DIRS)
 # STATICFILES_FINDERS = (
