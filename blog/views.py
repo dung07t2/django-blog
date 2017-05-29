01f5dd5e376f5e6d562f1d06d43da27681c30f8f
dunglh <dung07t2@gmail.com>
2017-05-29 22:32:19 +0800

    change context_instance

--- blog/views.py
+++ blog/views.py
@@ -18,29 +18,25 @@ from blog.utils.paginator import GenericPaginator
 
 
 def handler400(request):
-    response = render_to_response('error_page.html', {'title': '400 Bad Request', 'message': '400'},
-                                  context_instance=RequestContext(request))
+    response = render(request, 'error_page.html', {'title': '400 Bad Request', 'message': '400'})
     response.status_code = 400
     return response
 
 
 def handler403(request):
-    response = render_to_response('error_page.html', {'title': '403 Permission Denied', 'message': '403'},
-                                  context_instance=RequestContext(request))
+    response = render(request, 'error_page.html', {'title': '403 Permission Denied', 'message': '403'})
     response.status_code = 403
     return response
 
 
 def handler404(request):
-    response = render_to_response('error_page.html', {'title': '404 Not Found', 'message': '404'},
-                                  context_instance=RequestContext(request))
+    response = render(request, 'error_page.html', {'title': '404 Not Found', 'message': '404'})
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render_to_response('error_page.html', {'title': '500 Server Error', 'message': '500'},
-                                  context_instance=RequestContext(request))
+    response = render(request,'error_page.html', {'title': '500 Server Error', 'message': '500'})
     response.status_code = 500
     return response
 
